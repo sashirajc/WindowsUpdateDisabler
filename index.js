0a538b0fa8251152a8be6cab4eb51e10ce6c1d2b
sashirajc Remote <sashi@iconph.net>
2018-06-28 05:04:57 +0800

    Modified runas command

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const os = require('os');
 const cmd = require('node-cmd');
 
-
-if(os.type() === 'Windows_NT'){
-    cmd.get('sc query wuauserv',function(err,data){
-        var state = data.split('\n')[3];
-        if (state[29] === '4'){
-        cmd.get(`runas /user:Administrator sc stop wuauserv`,function(err,data){
-            // Figure out how to run as admin
-            console.log(err,'err2');
-            console.log(data,'data2');
-        })
+if (os.type() === 'Windows_NT') {
+  cmd.get('sc query wuauserv', function(err, data) {
+    var state = data.split('\n')[3];
+    if (state[29] === '4') {
+      cmd.get(`runas /user:Sash "sc stop wuauserv"`, function(err, data) {
+        // Figure out how to stop asking for password
+        console.log(err, 'err2');
+        console.log(data, 'data2');
+      });
     }
-    });
+  });
 }
