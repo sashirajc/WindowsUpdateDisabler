c3043cc700a7d3c0b8903d1f03a23b2a57147ae7
sashirajc Remote <sashi@iconph.net>
2018-07-05 22:49:41 +0800

    Added node-windows
    Updated code by switching from runas to node-windows elevate command.

--- WindowsUpdateBlocker.js
+++ WindowsUpdateBlocker.js
@@ -1,25 +1,25 @@
 const os = require('os');
 const cmd = require('node-cmd');
+const nodeWin = require('node-windows');
 const fs = require('fs');
 
 
 if (os.type() === 'Windows_NT') {
-  cmd.get('sc query wuauserv', function(err, data) {
-      if(err) writeLog(err);
-    var state = data.split('\n')[3];
-    if (state[29] === '4') {
-      cmd.get(`runas /user:Sash "sc stop wuauserv"`, function(err, data) {
-          if(err) writeLog(err);
-          else writeLog(data)
-      });
-    }
-  });
+    cmd.get('sc query wuauserv', function (err, data) {
+        if (err) writeLog(err);
+        var state = data.split('\n')[3];
+        if (state[29] === '4') {
+            nodeWin.elevate("sc stop wuauserv", function (err, data) {
+                if (err) writeLog(err);
+                else writeLog(data)
+            });
+        }
+    });
 }
 
-function writeLog(msg){
-    fs.appendFile('execLog.txt',msg,(err) => {
-        if(err)
-        console.log(err,'error');
-    }
-    );
+function writeLog(msg) {
+    fs.appendFile('execLog.txt', msg, (err) => {
+        if (err)
+            console.log(err, 'error');
+    });
 }
